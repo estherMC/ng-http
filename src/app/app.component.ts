7a0ed07418dc283bd656590ab8a5915147705d53
Esther Martorell <esther_3@hotmail.com>
2023-07-23 08:03:17 +0200

    :sparkles: send a post request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,11 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
+    this.http.post('https://ng-complete-guide-13a67-default-rtdb.europe-west1.firebasedatabase.app/posts.json', 
+    postData).subscribe(responseData => {
+      console.log(responseData);
+    })
+
   }
 
   onFetchPosts() {
