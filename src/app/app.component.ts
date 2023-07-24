338daf2d2ff122a866050776f0396c9e4623d11f
Esther Martorell <esther_3@hotmail.com>
2023-07-24 08:47:23 +0200

    :sparkles: use rxjs operator to transform response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -36,6 +37,16 @@ export class AppComponent implements OnInit {
 
   private fetchPosts(){
     this.http.get('https://ng-complete-guide-13a67-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
+    .pipe(map(responseData => {
+      const postsArray = [];
+      for (const key in responseData){
+        //Avoid accessing Prototype
+        if (responseData.hasOwnProperty(key)){
+          postsArray.push({...responseData[key], id: key});
+        }
+        return postsArray;
+      }
+    }))
     .subscribe((posts)=>{
       console.log(posts);
     });
