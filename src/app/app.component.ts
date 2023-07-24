734efbe64e10e9b57b739de4e4fceb3aa58af49d
Esther Martorell <esther_3@hotmail.com>
2023-07-24 14:53:13 +0200

    :sparkles: extract logic of http to service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Post } from './post.model';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
@@ -12,54 +13,25 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+              private postsService: PostsService) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    this.postsService.fetchPosts();
   }
 
   onCreatePost(postData: Post) {
     // Send Http request
-    console.log(postData);
-    this.http
-    .post<{name: string}>(
-      'https://ng-complete-guide-13a67-default-rtdb.europe-west1.firebasedatabase.app/posts.json', 
-    postData).subscribe(responseData => {
-      console.log(responseData);
-    })
-
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     // Send Http request
-    this.fetchPosts();
+    this.postsService.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
   }
 
-  private fetchPosts(){
-    this.isFetching=true;
-    this.http
-    .get<{[key: string]: Post }>('https://ng-complete-guide-13a67-default-rtdb.europe-west1.firebasedatabase.app/posts.json')
-    .pipe(map(
-      responseData => {
-        //The next is not needed to be defined:
-      // (responseData : {[key: string]: Post }) => {
-      const postsArray: Post[] = [];
-
-      for (const key in responseData){
-        //Avoid accessing Prototype
-        if (responseData.hasOwnProperty(key)){
-          postsArray.push({...responseData[key], id: key});
-        }
-      }
-      return postsArray;
-    }))
-    .subscribe((posts)=>{
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    });
-  }
 }
