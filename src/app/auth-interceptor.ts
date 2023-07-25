e877cb762c0fc1e883d284407dbe7f17f66094d2
Esther Martorell <esther_3@hotmail.com>
2023-07-25 15:39:17 +0200

    :sparkles: modify the url in interceptor

--- src/app/auth-interceptor.ts
+++ src/app/auth-interceptor.ts
@@ -4,7 +4,13 @@ export class AuthInterceptorService implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler){
         console.log('Request is on its way');
+        console.log(req.url);
+        //request is immutable, we need to clone
+        const modifiedRequest = req.clone({
+            //url: 'some-new-url'
+            headers: req.headers.append('Auth', 'xyz')
+        })
         //To allow to continue the request 
-        return next.handle(req);
+        return next.handle(modifiedRequest);
     }
 }
