7c52dc46e1db206ab94c3315d9d8cf71d5cf23df
Esther Martorell <esther_3@hotmail.com>
2023-07-25 14:57:58 +0200

    :sparkles: add an interceptor

---
+++ src/app/auth-interceptor.ts
@@ -0,0 +1,10 @@
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http'
+
+export class AuthInterceptorService implements HttpInterceptor {
+
+    intercept(req: HttpRequest<any>, next: HttpHandler){
+        console.log('Request is on its way');
+        //To allow to continue the request 
+        return next.handle(req);
+    }
+}
